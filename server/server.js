635c7ec364a2fc8eaabd9a0b565cb81677f02e91
abhilash <abhilashprasadsahoo2005@gmail.com>
2026-02-01 01:05:11 +0530

    added

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const attendanceRoutes = require('./routes/attendanceRoutes');
-const { getDB } = require('./mongoClient');
+const { getDB, connectToMongoDB } = require('./mongoClient');
 const bcrypt = require('bcryptjs');
 
 const app = express();
